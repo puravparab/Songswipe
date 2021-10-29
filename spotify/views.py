35fc6b4e8d25912fc8c8ccd13c26f58baa1f5165
PuravParab <47409476+PuravParab@users.noreply.github.com>
2021-10-29 20:19:19 +0400

    added spotify auth view

--- spotify/views.py
+++ spotify/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+# SPOTIFY API DOCUMENTAION:
+# https://developer.spotify.com/documentation/general/guides/authorization/code-flow/
 
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.conf import SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET
+from rest_framework.views import APIView
+from requests import Request
+
+# Request Authorization to access data
+class AuthSpotify(APIView):
+	def get(self, request, format=None):
+		scopes = '' 
+		url = Request('GET', 'https://accounts.spotify.com/authorize',
+			params={
+				'client_id': SPOTIFY_CLIENT_ID,
+				'response_type': 'code',
+				'redirect_uri': ,
+				'scope': scopes,
+			}).prepare().url
+		return redirect(url)
+
+# Callback function:
+# Spotify API redirects to this function (redirect_uri) after the user logs in
+ 
